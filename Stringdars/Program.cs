b42dd3dc12f72a0305f5c4189371414f8b6137d4
Mehmonov-Sherzod <mehmovovsherzod@gmail.com>
2025-04-10 23:25:47 +0500

    sozlardi eng kattasini topish

--- Stringdars/Program.cs
+++ Stringdars/Program.cs
@@ -5,23 +5,33 @@
     public static void Main(string[] args)
     {
         Console.Write("matn krit:");
-      string name = Console.ReadLine();
+        string name = Console.ReadLine();
 
 
-        string[] str = name.Split();
+        string[] str = name.Split(" ");
 
-        
-
-        for (int i = 0; i < str.Length; i++) {
+        string strt = " ";
 
 
-            Console.WriteLine(str[i]);
+        Console.Write(" ");
+        foreach (string st in str)
+        {
+
+            Console.WriteLine(st);
+
+            if (st.Length > strt.Length)
+            {
+
+                strt = st;
+            }
 
 
 
         }
 
 
+        Console.WriteLine(" eng kattasi:");
+        Console.Write(strt);
 
 
 
