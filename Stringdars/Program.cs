8ab6ef85c49233eafe5edb8376388f2f651aaeef
Mehmonov-Sherzod <mehmovovsherzod@gmail.com>
2025-04-10 23:02:54 +0500

    stringda misol ishladim

--- Stringdars/Program.cs
+++ Stringdars/Program.cs
@@ -5,10 +5,28 @@
     public static void Main(string[] args)
     {
 
+      string name = Console.ReadLine();
+
+        string str = " ";
+
+        for (int i = 0; i < name.Length; i++) {
+
+            if (name[i] == ' ')
+            {
 
 
+            }
+
+            else
+            {
+
+                str += name[i];
+            }
 
 
+        }
+
+        Console.WriteLine(str);
 
 
 
